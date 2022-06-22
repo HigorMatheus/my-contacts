c360e6e4ad2500286197618b7e9879b508d5e6d6
Higor Matheus <hmoliveira83@gmail.com>
2022-06-22 19:38:04 -0300

    finalizando api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.sendStatus(500);
+});
 
 app.listen(3333, () => console.log('🔥 server started at http://localhost:3333 '));
